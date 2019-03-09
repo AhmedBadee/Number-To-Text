d0149f6d4ab243b68ef97198b9db6a62c0909bfe
AhmedBadee <ahmedbadee17@gmail.com>
2019-03-09 17:25:54 +0200

    exported the new pipe

--- src/lib/number-to-text.module.ts
+++ src/lib/number-to-text.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import {NumberToTextPipe} from './pipes/number-to-text.pipe';
 import {NumberToTextArPipe} from './pipes/number-to-text-ar.pipe';
+import {NumberToTextArFemPipe} from './pipes/number-to-text-ar-fem.pipe';
 
 @NgModule({
   imports: [
   ],
-  declarations: [NumberToTextPipe, NumberToTextArPipe],
-  exports: [NumberToTextPipe, NumberToTextArPipe]
+  declarations: [NumberToTextPipe, NumberToTextArPipe, NumberToTextArFemPipe],
+  exports: [NumberToTextPipe, NumberToTextArPipe, NumberToTextArFemPipe]
 })
 export class NumberToTextModule { }
